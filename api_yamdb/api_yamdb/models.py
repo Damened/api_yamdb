5c95a3d53315c4692196c911d9f27f7621e209e3
Daniil Fedotov <pustovdanil@bk.ru>
2023-04-25 22:49:21 +0500

    changes in models for titles/categories/genres

--- api_yamdb/api_yamdb/models.py
+++ api_yamdb/api_yamdb/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 
 class Category(models.Model):
-    title = models.CharField(
-        max_length=20,
+    name = models.CharField( #исправил на name
+        max_length=256, #исправил на 256
         verbose_name='Название категории',
     )
     slug = models.SlugField(
+        max_length=50, #добавил длину
         unique=True,
         null=False,
         verbose_name='Раздел категории',
@@ -14,11 +15,12 @@ class Category(models.Model):
 
 
 class Genre(models.Model):
-    title = models.CharField(
-        max_length=20,
+    name = models.CharField( #исправил на name
+        max_length=256, #исправил
         verbose_name='Название жанра',
     )
     slug = models.SlugField(
+        max_length=50, #добавил
         unique=True,
         null=False,
         verbose_name='Раздел жанра',
@@ -26,21 +28,35 @@ class Genre(models.Model):
 
 
 class Title(models.Model):
-    title = models.CharField(
-        max_length=100,
+    name = models.CharField( #изменил на name
+        max_length=256, #добавил
         verbose_name='Название произведения',
     )
-    category = models.ForeignKey(
-        Category,
-        on_delete=models.SET_NULL,
-        verbose_name='Категория произведения',
+    year = models.IntegerField( #добавил
+        verbose_name='Год выпуска',
+    )
+    description = models.CharField( #добавил
+        verbose_name='Описание',
     )
     genre = models.ManyToManyField(
         Genre,
         through='GenreTitle',
     )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name='titles', #добавил
+        verbose_name='Категория произведения',
+    )
 
 
 class GenreTitle(models.Model):
-    genre = models.ForeignKey(Genre, on_delete=models.SET_NULL)
-    title = models.ForeignKey(Title, on_delete=models.SET_NULL)
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.SET_NULL,
+        related_name='titles', #добавил
+    )
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.SET_NULL,
+    )
