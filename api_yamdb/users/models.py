37d73e2194b5298f7fa1e3f46787331bfc050dc1
Ragnar Lodbrok <ragnar@MacBook-Air-Ragnar.local>
2023-04-25 16:57:01 +0500

    revision

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,35 +1,33 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from .userroles import UserRoles
+
 class User(AbstractUser):
     USER = 'user'
     MODERATOR = 'moderator'
     ADMIN = 'admin'
 
-    ROLES = [
-        (USER, 'user'),
-        (MODERATOR, 'moderator'),
-        (ADMIN, 'admin'),
-    ]
-
     Biography = models.TextField(
-        'Биография',
+        max_length=1000,
+        verbose_name='Биография',
         blank=True,
     )
     role = models.CharField(
-        max_length=10,
-        choices=ROLES,
-        default=USER,
+        max_length=100,
+        verbose_name='Роль',
+        choices=UserRoles.choices,
+        default=UserRoles.USER,
     )
 
     @property
     def is_admin(self):
-        return self.role == self.ADMIN
+        return self.role == UserRoles.ADMIN
 
     @property
     def is_moderator(self):
-        return self.role == self.MODERATOR
+        return self.role == UserRoles.MODERATOR
 
     @property
     def is_user(self):
-        return self.role == self.USER
+        return self.role == UserRoles.USER
