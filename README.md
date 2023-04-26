e99ae5bbf383353b92d264d989147b90a96f28be
Ragnar Lodbrok <ragnar@MacBook-Air-Ragnar.local>
2023-04-26 22:45:20 +0500

    token fix

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# api_yamdb
-api_yamdb
+Технологии:
+Python==3.9.10;
+Django==3.2.16;
+Django rest framework==3.12.4.
+Как запустить проект:
+Клонировать репозиторий и перейти в него в командной строке:
+
+git clone git@github.com:Damened/api_yamdb.git
+cd api_yamdb
+
+Cоздать и активировать виртуальное окружение:
+
+python3 -m venv venv
+source venv/Scripts/activate
+Установить зависимости из файла requirements.txt:
+
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+Выполнить миграции:
+
+python manage.py migrate
+Запустить проект:
+
+python manage.py runserver
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from datetime import timedelta
 
@@ -23,8 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken',
-    'djoser',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -43,7 +43,7 @@ TEMPLATES_DIR = BASE_DIR / 'templates'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -108,7 +108,7 @@ STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated', 
+        'rest_framework.permissions.IsAuthenticated',
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
