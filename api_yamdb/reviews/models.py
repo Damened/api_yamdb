eb6d2b583b5113508daace2c06ece9b0f7b58ce8
Daniil Fedotov <pustovdanil@bk.ru>
2023-04-28 18:26:51 +0500

    change models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -5,8 +5,28 @@ from django.db.models import Q
 User = get_user_model() # Временно пока не создана кастомная модель User
 
 
-class Title(models.Model): # Временно пока нет модели для произведений
-    pass
+class Title(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название произведения',
+    )
+    year = models.IntegerField(
+        verbose_name='Год выпуска',
+    )
+    description = models.TextField(
+        verbose_name='Описание',
+    )
+    genre = models.ManyToManyField(
+        'Genre',
+        through='GenreTitle',
+    )
+    category = models.ForeignKey(
+        'Category',
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        verbose_name='Категория произведения',
+    )
+
 
 class Review(models.Model):
     author = models.ForeignKey(
@@ -44,6 +64,41 @@ class Comment(models.Model):
     
     def __str__(self):
         return f'{self.text} комментарий к отзыву {self.review}'
-    
 
-    
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название категории',
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        null=False,
+        verbose_name='Раздел категории',
+    )
+
+
+class Genre(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название жанра',
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        null=False,
+        verbose_name='Раздел жанра',
+    )
+
+
+class GenreTitle(models.Model):
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.SET_NULL,
+        related_name='titles',
+    )
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.SET_NULL,
+    )
