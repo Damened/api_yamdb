9e6588e77029e9c5f8dc882177df96c4fb768bb3
Daniil Fedotov <pustovdanil@bk.ru>
2023-04-30 19:26:58 +0500

    Видит команду, работаю над ее содержанием

--- api_yamdb/reviews/management/commands/startimport.py
+++ api_yamdb/reviews/management/commands/startimport.py
@@ -9,14 +9,11 @@ PATH = f'{settings.BASE_DIR}/static/data/'
 
 class Command(BaseCommand):
 
-    def handle(self):
+    def handle(self, *args, **options):
         with open(PATH + 'category.csv', 'r', newline='') as csvfile:
             reader = csv.DictReader(csvfile)
             records = []
             for row in reader:
-                records.append(Category(
-                    id=row['id'],
-                    name=row['name'],
-                    slug=row['slug']))
+                records.append(Category(**row))
         Category.objects.bulk_create(records)
         self.stdout.write(self.style.SUCCESS('Данные импортированы.'))
