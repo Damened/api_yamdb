d1d7655f3b9dd5a8e78926953e1543cc1bc4f451
Daniil Fedotov <pustovdanil@bk.ru>
2023-05-01 01:57:17 +0500

    Добавил импорт из csv в БД, но есть проблема с User

--- api_yamdb/reviews/management/commands/startimport.py
+++ api_yamdb/reviews/management/commands/startimport.py
@@ -1,19 +1,42 @@
 import csv
-from django.core.management.base import BaseCommand, CommandError
-from django.conf import settings
-from reviews.models import Category
+from django.core.management.base import BaseCommand
+from reviews.models import Category, Genre, Title, User, Review, Comment, GenreTitle
 
 
-PATH = f'{settings.BASE_DIR}/static/data/'
+PATH = 'static/data/'
+FILES__MODELS = {
+    'category': Category,
+    'genre': Genre,
+    'titles': Title,
+    'users': User,
+    'review': Review,
+    'comments': Comment,
+    'genre_title': GenreTitle,
+}
 
 
 class Command(BaseCommand):
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--delete-existing',
+            action='store_true',
+            dest='delete_existing',
+            default=False,
+
+        )
+
     def handle(self, *args, **options):
-        with open(PATH + 'category.csv', 'r', newline='') as csvfile:
-            reader = csv.DictReader(csvfile)
-            records = []
-            for row in reader:
-                records.append(Category(**row))
-        Category.objects.bulk_create(records)
-        self.stdout.write(self.style.SUCCESS('Данные импортированы.'))
+        for file, model in FILES__MODELS.items():
+            with open(PATH + f'{file}.csv', 'r', newline='') as csvfile:
+                reader = csv.DictReader(csvfile)
+                records = []
+                for row in reader:
+                    records.append(model(**row))
+                    #  print(row) Можно посмотреть какие данные загружены в БД
+                if options['delete_existing']:
+                    model.objects.all().delete()
+                    self.stdout.write(self.style.SUCCESS('Предыдущие данные удалены.'))
+                model.objects.bulk_create(records)
+                self.stdout.write(self.style.SUCCESS('Данные импортированы.'))
+                csvfile.close()
