2a5ca8de0e44dff9ec22f0ed3837a5f3c0374be9
Daniil Fedotov <pustovdanil@bk.ru>
2023-05-01 02:15:59 +0500

    Добавил эндпоинты

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter 
 
-from .views import CommentViewSet, ReviewViewSet
+from .views import (CommentViewSet, ReviewViewSet, CategoryViewSet,
+                    GenreViewSet, TitleViewSet)
 
 router_v1 = DefaultRouter() 
 router_v1.register(r'titles/(?P<title_id>\d+)/reviews', 
@@ -9,8 +10,10 @@ router_v1.register(r'titles/(?P<title_id>\d+)/reviews',
                    basename='review')
 router_v1.register(r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments/', 
                    CommentViewSet, 
-                   basename='review') 
-
+                   basename='review')
+router_v1.register(r'categories/', CategoryViewSet, basename='category')
+router_v1.register(r'genres/', GenreViewSet, basename='genre')
+router_v1.register(r'titles/', TitleViewSet, basename='title')
 
 
 urlpatterns = [ 
