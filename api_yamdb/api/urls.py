ee979d364f3034f32aa7b454cb873744dab4c00e
Daniil Fedotov <pustovdanil@bk.ru>
2023-05-01 23:48:38 +0500

    Добавил эндпоинты для произведений, жанров и категорий

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter 
 
-from .views import CommentViewSet, ReviewViewSet
+from .views import (CommentViewSet, ReviewViewSet, TitleViewSet, CategoryViewSet,
+                    GenreViewSet,)
 
 router_v1 = DefaultRouter() 
 router_v1.register(r'titles/(?P<title_id>\d+)/reviews', 
@@ -9,10 +10,12 @@ router_v1.register(r'titles/(?P<title_id>\d+)/reviews',
                    basename='review')
 router_v1.register(r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments/', 
                    CommentViewSet, 
-                   basename='review') 
-
+                   basename='review')
+router_v1.register(r'titles/', TitleViewSet, basename='titles')
+router_v1.register(r'categories/', CategoryViewSet, basename='categories')
+router_v1.register(r'genres/', GenreViewSet, basename='genres')
 
 
 urlpatterns = [ 
     path('v1/', include(router_v1.urls), name='api-root'), 
-    ] 
+    ]
