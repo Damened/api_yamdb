42769dec58bb34ae4f669e1de21d74505512c150
RobbPear <120662039+RobbPear@users.noreply.github.com>
2023-05-04 01:15:34 +0500

    Update serializers.py
    Исправлен конфликт

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -4,13 +4,11 @@ from rest_framework.relations import SlugRelatedField
 from rest_framework.validators import UniqueTogetherValidator
 from django.shortcuts import get_object_or_404
 
-from reviews.models import Comment, Review, Category, Genre, Title, GenreTitle
-
+from reviews.models import Comment, Review, Category, Genre, Title
 from users.models import User
 
 
 class CategorySerializer(serializers.ModelSerializer):
-    """Сериализатор модели Category."""
 
     class Meta:
         fields = ('name', 'slug',)
@@ -18,7 +16,6 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class GenreSerializer(serializers.ModelSerializer):
-    """Сериализатор модели Genre."""
 
     class Meta:
         fields = ('name', 'slug',)
@@ -26,32 +23,15 @@ class GenreSerializer(serializers.ModelSerializer):
 
 
 class TitleSerializer(serializers.ModelSerializer):
-    """Сериализатор модели Title."""
     rating = serializers.SerializerMethodField()
-    genre = GenreSerializer(many=True)
-    category = CategorySerializer(many=False)
+    genre = GenreSerializer(many=True, read_only=True)
+    category = CategorySerializer(many=False, read_only=True)
 
     class Meta:
         fields = ('id', 'name', 'year', 'rating', 'description', 'genre',
                   'category',)
         read_only_fields = ('rating',)
         model = Title
-
-    def create(self, validated_data):
-        genres = validated_data.pop('genres')
-        category = validated_data.pop('category')
-        currents_genres = []
-        for genre in genres:
-            currents_genres.append(Genre.objects.get(slug=genre))
-        validated_data.append(currents_genres)
-        current_category = Category.objects.get(slug=category)
-        validated_data.append(current_category)
-        title = Title.objects.create(**validated_data)
-        for genre in currents_genres:
-            GenreTitle.objects.create(
-                genre=genre,
-                title=title,
-            )
 
     def get_rating(self, obj):
         reviews = obj.reviews.all()
