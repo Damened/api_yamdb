d4116324aff1ad577ff9bd3a0a58ad83e8deca5b
Александр Губайдуллин <robbpear@yandex.ru>
2023-05-03 23:58:48 +0500

    Не рабочий валидатор

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -65,6 +65,20 @@ class ReviewSerializer(serializers.ModelSerializer):
         if 0 > value > 10:
             raise serializers.ValidationError('Оценка должна быть от 1 до 10')
         return value
+    
+    # def validate(self, data):
+    #     request = self.context['request']
+    #     author = request.user
+    #     title_id = self.context.get('view').kwargs.get('title_id')
+    #     title = get_object_or_404(Title, pk=title_id)
+    #     if (
+    #         request.method == 'POST'
+    #         and Review.objects.filter(title=title, author=author).exists()
+    #     ):
+    #         raise serializers.ValidationError('Может существовать только один отзыв!')
+    #     return data
+    
+  
 
 
 class CommentSerializer(serializers.ModelSerializer): 
