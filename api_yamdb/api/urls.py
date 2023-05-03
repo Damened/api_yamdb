3fe794c0c1894f6c6d9a8d58c90ad25ed288aed1
Александр Губайдуллин <robbpear@yandex.ru>
2023-05-03 10:36:29 +0500

    сам не понял что поменял)))

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -21,3 +21,34 @@ router_v1.register(r'genres/', GenreViewSet, basename='genres')
 urlpatterns = [ 
     path('v1/', include(router_v1.urls), name='api-root'), 
     ]
+
+
+# from django.urls import include, path
+# from rest_framework.routers import DefaultRouter 
+
+# from .views import CommentViewSet, ReviewViewSet, GenreViewSet, TitleViewSet, CategoryViewSet
+
+# router_v1 = DefaultRouter() 
+# router_v1.register(r'titles/(?P<title_id>\d+)/reviews', 
+#                    ReviewViewSet, 
+#                    basename='reviews')
+# router_v1.register(r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments', 
+#                    CommentViewSet, 
+#                    basename='comments')
+# router_v1.register('genres', 
+#                    GenreViewSet, 
+#                    basename='genres')
+# router_v1.register('titles', 
+#                    TitleViewSet, 
+#                    basename='titles')
+# router_v1.register('categories', 
+#                    CategoryViewSet, 
+#                    basename='categories')
+
+# # http://127.0.0.1:8000/api/v1/titles/{title_id}/reviews/{review_id}/comments/{comment_id}/
+# # http://127.0.0.1:8000/api/v1/categories/
+# # http://127.0.0.1:8000/api/v1/genres/
+
+# urlpatterns = [ 
+#     path('v1/', include(router_v1.urls), name='api-root'), 
+#     ] 
