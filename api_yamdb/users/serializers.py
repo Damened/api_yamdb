2b0c4d974ef1ed9541ee87827ccfebc1fad99023
Александр Губайдуллин <robbpear@yandex.ru>
2023-05-05 19:14:51 +0500

    72 теста пройдены

--- api_yamdb/users/serializers.py
+++ api_yamdb/users/serializers.py
@@ -8,7 +8,20 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = 'username', 'email', 'first_name', 'last_name', 'bio', 'role'
+        
 
+###############
+
+class NotAdminSerializer(serializers.ModelSerializer):
+        
+    class Meta:
+        model = User
+        fields = (
+            'username', 'email', 'first_name',
+            'last_name', 'bio', 'role')
+        read_only_fields = ('role',)
+
+###########################
 
 class SignUpUserSerializer(serializers.Serializer):
     """Сериализатор регистрации пользователей"""
