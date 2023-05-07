12842d298d45d630c1d2e0272d6827e0dabbbbfa
Ragnar Lodbrok <ragnar@MacBook-Air-Ragnar.local>
2023-05-07 19:03:41 +0500

    error-pull

--- api_yamdb/reviews/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class ReviewsConfig(AppConfig):
-    # строка создана при создании приложения без нее возникают ошибки с авто-заполняемыми полями
-    default_auto_field = 'django.db.models.BigAutoField' 
-    name = 'reviews'
-    verbose_name = 'Отзывы'
